0132c6f83231910e9dd1b6e96bd23ef1aebe0050
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-10-25 23:29:59 -0400

    FEAT: Grid file created for GOL

---
+++ grid.py
@@ -0,0 +1,24 @@
+"""
+TODO:
+- 2D list
+    - colums, rows
+
+Neighborhood:
+[ ][ ][ ]
+[ ][x][ ]
+[ ][ ][ ]
+"""
+
+
+class Grid:
+    def __init__(self, width, height):
+        self.columns = height
+        self.rows = width
+
+    def make_list(self):
+        for x in range(self.rows):
+            for y in range(self.columns):
+                pass
+
+    def setup(self):
+        # grid = make_list(10, 10)
