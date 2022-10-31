ec4ae827aa2dd9b0d26c405075f0e7df9a8a3ae8
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-10-31 18:17:30 -0400

    FEAT-GRID: nametuple used

--- grid.py
+++ grid.py
@@ -1,4 +1,9 @@
-import pygame
+from collections import namedtuple
+
+Dimension = namedtuple("Dimension", ["width", "height"])
+Grid = namedtuple("Grid", ["dimension", "cells"])
+Neighbors = namedtuple("Neighbors", ["alive", "dead"])
+
 
 """
 TODO:
@@ -12,21 +17,21 @@ Neighborhood:
 """
 
 
-class Grid:
-    def __init__(self, width, height, cells):
-        self.height = height
-        self.width = width
-        self.cells = cells
-        # self.window = window
+# class Grid:
+#     def __init__(self, width, height, cells):
+#         self.height = height
+#         self.width = width
+#         self.cells = cells
+# self.window = window
 
-    # def make_list(self):
-    #     for x in range(self.width):
-    #         for y in range(self.height):
-    #             pass
+# def make_list(self):
+#     for x in range(self.width):
+#         for y in range(self.height):
+#             pass
 
-    # def setup(self, width, height, cells=30):
-    #     window = pygame.display.set_mode(width, height)
-    #     for x in range(0, width, cells):
-    #         for y in range(0, height, cells):
-    #             rect = pygame.Rect(x, y, cells, cells)
-    #             pygame.draw.rect(window, (255, 255, 255), rect, 1)
+# def setup(self, width, height, cells=30):
+#     window = pygame.display.set_mode(width, height)
+#     for x in range(0, width, cells):
+#         for y in range(0, height, cells):
+#             rect = pygame.Rect(x, y, cells, cells)
+#             pygame.draw.rect(window, (255, 255, 255), rect, 1)
